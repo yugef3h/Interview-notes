5c5855e2742eb45279967914170b05b475755780
yugef3h <245868066@qq.com>
2019-08-23 10:55:09 +0800

    数组处理

---
+++ 手写代码/high-frequency/基础数组排序.js
@@ -0,0 +1,39 @@
+// 冒泡
+function bubbleSort(arr) {
+  let len = arr.length
+  for (let i=len; i>= 2; i--) { // 排完第 2 个，第一个自动为最小
+    for (let j=0; j<i-1; j++) { // 每轮排好，后面的都就绪，自然要缩小范围，只排前面
+      if (arr[j] > arr[j+1]) {
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+      }
+    }
+  }
+  return arr
+}
+
+// 选择排序：遍历自身以后的元素，最小的元素跟自己调换位置
+function selectSort(arr) {
+  let len = arr.length
+  for (let i=0; i<len-1; i++) {
+    for (let j=i; j<len; j++) {
+      if (arr[j] < arr[i]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]]
+      }
+    }
+  }
+  return arr
+}
+
+// 插入排序: 即将元素插入到已排序好的数组中
+function insertSort(arr) {
+  for (let i=1; i<arr.length; i++) { // 从 1 开始，默认 0 为已排序
+    for (let j=i; j>0; j--) {
+      if (arr[j] < arr[j-1]) {
+        [arr[j],arr[j-1]] = [arr[j-1],arr[j]]
+      } else {
+        break;
+      }
+    }
+  }
+  return arr
+}
---
+++ 手写代码/high-frequency/数组去重.js
@@ -0,0 +1,34 @@
+// 数组去重可以直接使用 Array.from(new Set(arr))
+// 目前最快、数据过滤最佳的方法 Map
+// hash 字典，创建一个 obj
+function hashUnique(a, b) {
+  let arr = a.concat(b)
+  let result = []
+  let obj = {}
+  for (let i of arr) {
+      if (!obj[i]) {
+          result.push(i)
+          obj[i] = 1
+      }
+  }
+  return result
+}
+// map
+Array.prototype.mapUnique = function() {
+  // 1
+  const newArray = []
+  const tmp = new Map()
+  for (let i=0; i<this.length; i++) {
+    if (!tmp.get(this[i])) {
+      tmp.set(this[i], 1)
+      newArray.push(this[i])
+    }
+  }
+  return newArray
+
+  // 2
+  const tmp = new Map()
+  return this.filter(item => {
+    return !tmp.has(item) && tmp.set(item, 1)
+  })
+}
