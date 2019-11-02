78210c1b4d80eafb031286293e7a8d11540e54af
yugef3h <245868066@qq.com>
2019-11-02 15:59:27 +0800

    复写 new 及相应原理

--- 手写代码/high-frequency/new.js
+++ 手写代码/high-frequency/new.js
@@ -23,3 +23,23 @@ console.log(person.strength) // 60
 console.log(person.age) // 18
 
 // res = ctor.apply(obj, args) return res
+
+function myNew(Func, ...args) {
+  const instance = {}
+  if (Func.prototype) Object.setPrototypeOf(instance, Func.prototype)
+
+  const res = Func.apply(instance, args)
+  if (typeof res === 'function' || (typeof res === 'object' && res !== null)) return res
+  return instance
+}
+
+// 测试
+function Person(name) {
+  this.name = name
+}
+Person.prototype.sayName = function() {
+  console.log(`My name is ${this.name}`)
+}
+const me = myNew(Person, 'Jack')
+me.sayName()
+console.log(me)
---
+++ 手写代码/high-frequency/vue reactive.js
@@ -0,0 +1,86 @@
+// Dep module
+class Dep {
+  static stack = []
+  static target = null
+  deps = null
+  
+  constructor() {
+    this.deps = new Set()
+  }
+
+  depend() {
+    if (Dep.target) {
+      this.deps.add(Dep.target)
+    }
+  }
+
+  notify() {
+    this.deps.forEach(w => w.update())
+  }
+
+  static pushTarget(t) {
+    if (this.target) {
+      this.stack.push(this.target)
+    }
+    this.target = t
+  }
+
+  static popTarget() {
+    this.target = this.stack.pop()
+  }
+}
+
+// reactive
+function reactive(o) {
+  if (o && typeof o === 'object') {
+    Object.keys(o).forEach(k => {
+      defineReactive(o, k, o[k])
+    })
+  }
+  return o
+}
+
+function defineReactive(obj, k, val) {
+  let dep = new Dep()
+  Object.defineProperty(obj, k, {
+    get() {
+      dep.depend()
+      return val
+    },
+    set(newVal) {
+      val = newVal
+      dep.notify()
+    }
+  })
+  if (val && typeof val === 'object') {
+    reactive(val)
+  }
+}
+
+// watcher
+class Watcher {
+  constructor(effect) {
+    this.effect = effect
+    this.update()
+  }
+
+  update() {
+    Dep.pushTarget(this)
+    this.value = this.effect()
+    Dep.popTarget()
+    return this.value
+  }
+}
+
+// 测试代码
+const data = reactive({
+  msg: 'aaa'
+})
+
+new Watcher(() => {
+  console.log('===> effect', data.msg);
+})
+
+setTimeout(() => {
+  data.msg = 'hello'
+}, 1000)
