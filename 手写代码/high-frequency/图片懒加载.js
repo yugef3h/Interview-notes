78ae01bc9756ee5bd94ae08f839a71a406a45a07
yugef3h <245868066@qq.com>
2019-12-06 16:05:22 +0800

    dom transform

---
+++ 手写代码/high-frequency/图片懒加载.js
@@ -0,0 +1,28 @@
+// <img src="default.png" data-src="https://xxxx/real.png">
+function isVisible(el) {
+  const position = el.getBoundingClientRect()
+  const windowHeight = document.documentElement.clientHeight
+  // 顶部边缘可见
+  const topVisible = position.top > 0 && position.top < windowHeight;
+  // 底部边缘可见
+  const bottomVisible = position.bottom < windowHeight && position.bottom > 0;
+  return topVisible || bottomVisible;
+}
+
+function imageLazyLoad() {
+  const images = document.querySelectorAll('img')
+  for (let img of images) {
+    const realSrc = img.dataset.src
+    if (!realSrc) continue
+    if (isVisible(img)) {
+      img.src = realSrc
+      img.dataset.src = ''
+    }
+  }
+}
+
+// 测试
+window.addEventListener('load', imageLazyLoad)
+window.addEventListener('scroll', imageLazyLoad)
+// or
+window.addEventListener('scroll', throttle(imageLazyLoad, 1000))
---
+++ 手写代码/high-frequency/将VirtualDom转化为真实DOM结构.js
@@ -0,0 +1,33 @@
+// vnode结构：
+// {
+//   tag,
+//   attrs,
+//   children,
+// }
+
+//Virtual DOM => DOM
+function render(vnode, container) {
+  container.appendChild(_render(vnode));
+}
+function _render(vnode) {
+  // 如果是数字类型转化为字符串
+  if (typeof vnode === 'number') {
+    vnode = String(vnode);
+  }
+  // 字符串类型直接就是文本节点
+  if (typeof vnode === 'string') {
+    return document.createTextNode(vnode);
+  }
+  // 普通DOM
+  const dom = document.createElement(vnode.tag);
+  if (vnode.attrs) {
+    // 遍历属性
+    Object.keys(vnode.attrs).forEach(key => {
+      const value = vnode.attrs[key];
+      dom.setAttribute(key, value);
+    })
+  }
+  // 子数组进行递归操作
+  vnode.children.forEach(child => render(child, dom));
+  return dom;
+}
