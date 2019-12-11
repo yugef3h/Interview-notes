a572a68839f36ce1bc97cfd33a17cd4e5472730d
yugef3h <245868066@qq.com>
2019-12-11 16:05:49 +0800

    深拷贝修正

--- 手写代码/high-frequency/深拷贝.js
+++ 手写代码/high-frequency/深拷贝.js
@@ -1,2 +1,52 @@
 // JSON.parse(JSON.stringify())
 // 无法解决：循环引用，会导致栈溢出；特殊对象如 RegExp、Date、Set、Map 及函数。
+
+
+function deepCopy(obj, cache=new WeakMap()) {
+  if (!obj instanceof Object) return obj
+  // 防止循环引用
+  if (cache.get(obj)) return cache.get(obj)
+  // 支持函数
+  if (obj instanceof Function) {
+    return function() {
+      return obj.apply(this, arguments)
+    }
+  }
+  // 支持日期
+  if (obj instanceof Date) return new Date(obj)
+  // 支持正则对象
+  if (obj instanceof RegExp) return new RegExp(obj.source, obj.flags)
+
+  // 还可以增加其他对象，比如：Map, Set等，根据情况判断增加即可，面试点到为止就可以了
+
+  // 数组是 key 为数字的特殊对象
+  const res = Array.isArray(obj) ? [] : {}
+  // 缓存 copy 的对象，用于处理循环引用的情况
+  cache.set(obj, res)
+
+  Object.keys(obj).forEach(key => {
+    if (obj[key] instanceof Object) {
+      res[key] = deepCopy(obj[key], cache)
+    } else {
+      res[key] = obj[key]
+    }
+  })
+  return res
+}
+
+// 测试
+const source = {
+  name: 'Jack',
+  meta: {
+    age: 12,
+    birth: new Date('1997-10-10'),
+    ary: [1, 2, { a: 1 }],
+    say() {
+      console.log('Hello');
+    }
+  }
+}
+source.source = source
+const newObj = deepCopy(source)
+console.log(newObj.meta.ary[2] === source.meta.ary[2]); // false
+console.log(newObj.meta.birth === source.meta.birth); // false
