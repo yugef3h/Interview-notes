18a4b9d74ca85acee20f412eb12b26f78735ff89
yugef3h <245868066@qq.com>
2019-12-18 16:06:37 +0800

    ajax 手写代码

---
+++ 手写代码/high-frequency/AJAX.js
@@ -0,0 +1,16 @@
+const getJSON = function(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Mscrosoft.XMLHttp');
+    xhr.open('GET', url, false);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== 4) return;
+      if (xhr.status === 200 || xhr.status === 304) {
+        resolve(xhr.responseText);
+      } else {
+        reject(new Error(xhr.responseText));
+      }
+    }
+    xhr.send();
+  })
+}
