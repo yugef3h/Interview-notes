ae5cb271d9ddd6fa6b33f93e6eaef7c8f752ff56
yugef3h <245868066@qq.com>
2019-12-20 16:07:21 +0800

    异步高频考题

---
+++ 手写代码/high-frequency/异步串行 | 异步并行.js
@@ -0,0 +1,44 @@
+// 字节面试题，实现一个异步加法
+function asyncAdd(a, b, callback) {
+  setTimeout(function () {
+    callback(null, a + b);
+  }, 500);
+}
+
+// 解决方案
+// 1. promisify
+const promiseAdd = (a, b) => new Promise((resolve, reject) => {
+  asyncAdd(a, b, (err, res) => {
+    if (err) {
+      reject(err)
+    } else {
+      resolve(res)
+    }
+  })
+})
+
+// 2. 串行处理
+async function serialSum(...args) {
+  return args.reduce((task, now) => task.then(res => promiseAdd(res, now)), Promise.resolve(0))
+}
+
+// 3. 并行处理
+async function parallelSum(...args) {
+  if (args.length === 1) return args[0]
+  const tasks = []
+  for (let i = 0; i < args.length; i += 2) {
+    tasks.push(promiseAdd(args[i], args[i + 1] || 0))
+  }
+  const results = await Promise.all(tasks)
+  return parallelSum(...results)
+}
+
+// 测试
+(async () => {
+  console.log('Running...');
+  const res1 = await serialSum(1, 2, 3, 4, 5, 8, 9, 10, 11, 12)
+  console.log(res1)
+  const res2 = await parallelSum(1, 2, 3, 4, 5, 8, 9, 10, 11, 12)
+  console.log(res2)
+  console.log('Done');
+})()
