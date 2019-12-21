071df3d3128c4241e8de2537af89d0e8ca520545
yugef3h <245868066@qq.com>
2019-12-21 16:07:51 +0800

    fix 寄生组合模式

--- 手写代码/high-frequency/寄生组合继承.js
+++ 手写代码/high-frequency/寄生组合继承.js
@@ -11,3 +11,40 @@ Child.prototype.constructor = Child
 Object.setPrototypeOf(Child, Parent) // 继承父类的静态方法。
 
 // ES6  extends
+
+
+
+
+
+
+function create(proto) {
+  function F() {}
+  F.prototype = proto;
+  return new F();
+}
+
+// Parent
+function Parent(name) {
+  this.name = name
+}
+
+Parent.prototype.sayName = function () {
+  console.log(this.name)
+};
+
+// Child
+function Child(age, name) {
+  Parent.call(this, name)
+  this.age = age
+}
+Child.prototype = create(Parent.prototype)
+Child.prototype.constructor = Child
+
+Child.prototype.sayAge = function () {
+  console.log(this.age)
+}
+
+// 测试
+const child = new Child(18, 'Jack')
+child.sayName()
+child.sayAge()
