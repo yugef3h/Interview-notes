39ea81f08d4baec167a696b0a8deac9ea1f538c0
yugef3h <245868066@qq.com>
2019-12-22 16:08:23 +0800

    event emitter commit

---
+++ 手写代码/high-frequency/Promise 并行限制.js
@@ -0,0 +1,47 @@
+class Scheduler {
+  constructor() {
+    this.queue = [];
+    this.maxCount = 2;
+    this.runCounts = 0;
+  }
+  add(promiseCreator) {
+    this.queue.push(promiseCreator);
+  }
+  taskStart() {
+    for (let i = 0; i < this.maxCount; i++) {
+      this.request();
+    }
+  }
+  request() {
+    if (!this.queue || !this.queue.length || this.runCounts >= this.maxCount) {
+      return;
+    }
+    this.runCounts++;
+
+    this.queue.shift()().then(() => {
+      this.runCounts--;
+      this.request();
+    });
+  }
+}
+   
+const timeout = time => new Promise(resolve => {
+  setTimeout(resolve, time);
+})
+  
+const scheduler = new Scheduler();
+  
+const addTask = (time,order) => {
+  scheduler.add(() => timeout(time).then(()=>console.log(order)))
+}
+  
+  
+addTask(1000, '1');
+addTask(500, '2');
+addTask(300, '3');
+addTask(400, '4');
+scheduler.taskStart()
+// 2
+// 3
+// 1
+// 4
---
+++ 手写代码/high-frequency/eventEmitter.js
@@ -0,0 +1,69 @@
+function EventEmitter() {
+  this.events = new Map();
+}
+
+// 需要实现的一些方法：
+// addListener、removeListener、once、removeAllListeners、emit
+
+// 模拟实现addlistener方法
+const wrapCallback = (fn, once = false) => ({ callback: fn, once });
+EventEmitter.prototype.addListener = function(type, fn, once = false) {
+  const hanlder = this.events.get(type);
+  if (!hanlder) {
+    // 没有type绑定事件
+    this.events.set(type, wrapCallback(fn, once));
+  } else if (hanlder && typeof hanlder.callback === 'function') {
+    // 目前type事件只有一个回调
+    this.events.set(type, [hanlder, wrapCallback(fn, once)]);
+  } else {
+    // 目前type事件数>=2
+    hanlder.push(wrapCallback(fn, once));
+  }
+}
+// 模拟实现removeListener
+EventEmitter.prototype.removeListener = function(type, listener) {
+  const hanlder = this.events.get(type);
+  if (!hanlder) return;
+  if (!Array.isArray(this.events)) {
+    if (hanlder.callback === listener.callback) this.events.delete(type);
+    else return;
+  }
+  for (let i = 0; i < hanlder.length; i++) {
+    const item = hanlder[i];
+    if (item.callback === listener.callback) {
+      hanlder.splice(i, 1);
+      i--;
+      if (hanlder.length === 1) {
+        this.events.set(type, hanlder[0]);
+      }
+    }
+  }
+}
+// 模拟实现once方法
+EventEmitter.prototype.once = function(type, listener) {
+  this.addListener(type, listener, true);
+}
+// 模拟实现emit方法
+EventEmitter.prototype.emit = function(type, ...args) {
+  const hanlder = this.events.get(type);
+  if (!hanlder) return;
+  if (Array.isArray(hanlder)) {
+    hanlder.forEach(item => {
+      item.callback.apply(this, args);
+      if (item.once) {
+        this.removeListener(type, item);
+      }
+    })
+  } else {
+    hanlder.callback.apply(this, args);
+    if (hanlder.once) {
+      this.events.delete(type);
+    }
+  }
+  return true;
+}
+EventEmitter.prototype.removeAllListeners = function(type) {
+  const hanlder = this.events.get(type);
+  if (!hanlder) return;
+  this.events.delete(type);
+}
