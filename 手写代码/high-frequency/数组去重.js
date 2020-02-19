6abb2de8958ef0017fc59e49027db885cd06bf28
yugef3h <245868066@qq.com>
2020-02-19 10:54:09 +0800

    refactor:尝试和比较数组去重的效率

--- 手写代码/high-frequency/数组去重.js
+++ 手写代码/high-frequency/数组去重.js
@@ -32,3 +32,34 @@ Array.prototype.mapUnique = function() {
     return !tmp.has(item) && tmp.set(item, 1)
   })
 }
+
+var array = [1, 1, '1', '1'];
+
+function unique(array) {
+    var res = [];
+    for (var i = 0, arrayLen = array.length; i < arrayLen; i++) {
+        for (var j = 0, resLen = res.length; j < resLen; j++ ) {
+            if (array[i] === res[j]) {
+                break;
+            }
+        }
+        if (j === resLen) {
+            res.push(array[i])
+        }
+    }
+    return res;
+}
+
+console.log(unique(array)); // [1, "1"]
+
+
+function uniqueByIndexOf(array) {
+  var res = [];
+  for (var i = 0, len = array.length; i < len; i++) {
+      var current = array[i];
+      if (res.indexOf(current) === -1) {
+          res.push(current)
+      }
+  }
+  return res;
+}
