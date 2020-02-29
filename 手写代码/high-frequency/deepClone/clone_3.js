b38267c3a1cce222d1da3f9f397d949c45163988
yugef3h <245868066@qq.com>
2020-02-29 10:56:07 +0800

    feat:手写深拷贝

---
+++ 手写代码/high-frequency/deepClone/clone_3.js
@@ -0,0 +1,15 @@
+module.exports = function clone(target, map = new WeakMap()) {
+  if (typeof target === 'object') {
+      let cloneTarget = Array.isArray(target) ? [] : {};
+      if (map.get(target)) {
+          return target;
+      }
+      map.set(target, cloneTarget);
+      for (const key in target) {
+          cloneTarget[key] = clone(target[key], map);
+      }
+      return cloneTarget;
+  } else {
+      return target;
+  }
+};
