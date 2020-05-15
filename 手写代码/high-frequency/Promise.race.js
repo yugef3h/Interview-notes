dcb4898e96521278eecaa6cfb2d74ff605c1942f
yugef3h <245868066@qq.com>
2020-05-15 11:02:59 +0800

    feat:Promise.race

--- 手写代码/high-frequency/Promise.race.js
+++ 手写代码/high-frequency/Promise.race.js
@@ -9,3 +9,31 @@ Promise.race = function(promiseArr) {
     })
   })
 }
+
+// 2
+Promise.race = function (iterators) {  
+  return new Promise((resolve,reject) => {
+      for (const p of iterators) {
+          Promise.resolve(p)
+          .then((res) => {
+              resolve(res)
+          })
+          .catch(e => {
+              reject(e)
+          })
+      }
+  })
+
+}
+var promise1 = new Promise(function(resolve, reject) {
+  setTimeout(resolve, 500, 'one');
+});
+
+var promise2 = new Promise(function(resolve, reject) {
+  setTimeout(resolve, 100, 'two');
+});
+
+Promise.race([promise1, promise2]).then(function(value) {
+  console.log(value);
+  // Both resolve, but promise2 is faster
+});
