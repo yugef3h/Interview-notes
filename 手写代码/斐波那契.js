ed3e9a68b9b29a21508636830e010b45a1355751
yugef3h <245868066@qq.com>
2021-01-12 09:52:11 +0800

    refactor:fibonacci updated

--- 手写代码/斐波那契.js
+++ 手写代码/斐波那契.js
@@ -1,7 +1,17 @@
-function cStairs(n) {
+// 错误
+function fibonacci(n) {
   if(n === 1 || n === 2) {
       return 1;
   } else {
-      return cStairs(n-1) + cStairs(n-2)
+      return fibonacci(n-1) + fibonacci(n-2)
   }
+}
+
+// 更新
+const fib = function(n) {
+  let fibonacci = [0, 1]
+  for (let i = 2; i <= n; i++) {
+    fibonacci[i] = (fibonacci[i - 1] + fibonacci[i - 2]) % (1e9 + 7)
+  }
+  return fibonacci[n]
 }
